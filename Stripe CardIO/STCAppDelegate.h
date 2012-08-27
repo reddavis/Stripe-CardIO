09a45ddd101a65ced37982ddb4b601d8d6669ed0
Red <red@riothq.com>
2012-08-27 11:54:16 +0100

    Initial Commit

---
+++ Stripe CardIO/STCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STCAppDelegate.h
+//  Stripe CardIO
+//
+//  Created by Red Davis on 27/08/2012.
+//  Copyright (c) 2012 Red Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stripe CardIO/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stripe CardIO
+//
+//  Created by Red Davis on 27/08/2012.
+//  Copyright (c) 2012 Red Davis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STCAppDelegate class]));
+    }
+}
